1efde7dd5ccf51d0314f07fb129c4edcec7ccf21
Jack <user.name>
2020-05-16 14:00:17 -0700

    More plots

--- training results/ggplot_graphs.R
+++ training results/ggplot_graphs.R
@@ -43,15 +43,60 @@ data <- lapply(seq_along(data), function(index) {
 # Combine all of the dataframes into a master df
 df <- data %>% reduce(full_join)
 
-# Compare all of the networks
+# Compare all of the networks' losses
 ggplot(data = df, aes(x = X, y = loss, color=filename)) +
   theme(
     panel.background = element_rect(fill = "white", color = "white",  size = 0.5, linetype = "solid"),
     panel.grid.major = element_line(size = 0.25, linetype = 'dashed', color = "black"),
     panel.grid.minor = element_line(size = 0.1, linetype = 'dotted', color = "black")
   ) +
-  geom_line(size = 1) +
   xlab("Epoch") +
   ylab("Loss") +
-  guides(color=guide_legend(title="Network"))
+  guides(color=guide_legend(title="Network")) +
+  facet_grid(filename ~ ., scales = "free_y") +
+  geom_line(size = 1) +
+  geom_line(aes(x = X, y = val_loss, color=filename), size = 1, linetype = "dashed")
+
+ggplot(data = df, aes(x = X, y = acc, color=filename)) +
+  theme(
+    panel.background = element_rect(fill = "white", color = "white",  size = 0.5, linetype = "solid"),
+    panel.grid.major = element_line(size = 0.25, linetype = 'dashed', color = "black"),
+    panel.grid.minor = element_line(size = 0.1, linetype = 'dotted', color = "black")
+  ) +
+  xlab("Epoch") +
+  ylab("Loss") +
+  guides(color=guide_legend(title="Network")) +
+  facet_grid(filename ~ ., scales = "free_y") +
+  geom_line(size = 1) +
+  geom_line(aes(x = X, y = val_acc, color=filename), size = 1, linetype = "dashed")
+
+
+
+
+
+
+
+# Facet by training vs loss
+dfs <- lapply(data, function(df) {
+  gather(df, "set", "loss", -X, -acc, -val_acc, -filename)
+})
+# Add filenames as a column to each dataframe
+dfs <- lapply(seq_along(dfs), function(index) {
+  df$filename <- rep(files[[index]], nrow(data[[index]]))
+})
+
+# Combine all of the dataframes into a master df
+dfs <- dfs %>% reduce(full_join)
+
+ggplot(data = dfs, aes(x = X, y = loss, color=set)) +
+  theme(
+    panel.background = element_rect(fill = "white", color = "white",  size = 0.5, linetype = "solid"),
+    panel.grid.major = element_line(size = 0.25, linetype = 'dashed', color = "black"),
+    panel.grid.minor = element_line(size = 0.1, linetype = 'dotted', color = "black")
+  ) +
+  xlab("Epoch") +
+  ylab("Loss") +
+  guides(color=guide_legend(title="Network")) +
+  facet_grid(filename ~ ., scales = "free_y") +
+  geom_line(size = 1)
 
