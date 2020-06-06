4c2e893817edd2cdd1ae1bb7b843e97570911eb3
marqcordova <44125436+marqcordova@users.noreply.github.com>
2020-06-05 20:05:37 -0700

    Update train_mobilenet.py

--- train_mobilenet.py
+++ train_mobilenet.py
@@ -19,7 +19,7 @@ path_to_images = os.path.join('C:\\Users\\cordovam\\Desktop\\project 2')
 train_dir = os.path.join(path_to_images, 'project_train')
 val_dir = os.path.join(path_to_images, 'project_val')
 
-#Raths to individual class folders
+#Paths to individual class folders
 train_0_dir = os.path.join(train_dir, '0')
 train_1_dir = os.path.join(train_dir, '1')
 val_0_dir = os.path.join(val_dir, '0')
@@ -52,6 +52,7 @@ train_image_generator = ImageDataGenerator(rescale=1./255,
 # Generator for our validation data
 validation_image_generator = ImageDataGenerator(rescale=1./255) 
 
+#We're going to use flow from directory, because this is too big for ram
 train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
                                                            directory=train_dir,
                                                            shuffle=True,
@@ -62,34 +63,38 @@ val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_s
                                                               directory=val_dir,
                                                               target_size=(IMG_HEIGHT, IMG_WIDTH),
                                                               class_mode="binary")
-
+#take a look at one 
 sample_training_images, labs = next(train_data_gen)
-#############PICK A NETWORK
+#Import the mobilenet model or ANY OTHER MODEL
+#base_model = tf.keras.applications.ResNet50(input_shape=(IMG_HEIGHT, IMG_WIDTH, 3),
+#base_model = tf.keras.applications.MobileNet(input_shape=(IMG_HEIGHT, IMG_WIDTH, 3),
 base_model = tf.keras.applications.MobileNetV2(input_shape=(IMG_HEIGHT, IMG_WIDTH, 3),
                                                include_top=False,
                                                weights='imagenet')
 
 #########################
-
+#We need to add a layer before the prediction layer
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D(
     data_format='channels_last')
 
+#add a dense prediction layer
 prediction_layer = tf.keras.layers.Dense(1)
 
-
+#stack up the model
 model = tf.keras.Sequential([
   base_model,
   global_average_layer,
   prediction_layer
 ])
 
+#set the learning rate to start
 base_learning_rate = 0.0001
 
 model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-#add callback to save best model based on val_acc
+#add callback to save best model based on val_acc and lower learning rate
 my_callbacks = [
     #tf.keras.callbacks.EarlyStopping(patience=2),
     tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5',
@@ -101,7 +106,7 @@ my_callbacks = [
                                          patience=5)
 ]
 
-
+#Train the model
 history = model.fit_generator(
     train_data_gen,
     steps_per_epoch= total_train // batch_size,
@@ -111,6 +116,7 @@ history = model.fit_generator(
     validation_steps= total_val // batch_size
 )
 
+#Save metrics for later plotting
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -124,18 +130,3 @@ filename = 'mobilenet.csv'
 pd.DataFrame(data = np.column_stack((acc, val_acc, loss, val_loss)), 
              columns = ['acc', 'val_acc', 'loss', 'val_loss'], 
              index = list(epochs_range)).to_csv(os.path.join(path_to_images,filename))
-
-#plots that we want to look at and save?, but we'll make better ones in R
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
